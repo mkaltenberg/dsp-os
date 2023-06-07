0184284bba1e8e4b483c000df8542b63cd6a80f4
matthew <matthew@kaltenberg.io>
2023-06-07 14:25:01 -0500

    updated amplify backend files

---
+++ applications/fieldscan/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "diversifiedseedos": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
